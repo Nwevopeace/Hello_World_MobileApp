80fae77153ba73e7947daa4319137d0540843cad
Nwevo Peace Chinedu <nwevopeacechinedu@gmail.com>
2021-04-20 14:49:04 -0700

    Initial commit

---
+++ app/src/main/java/com/peacecodes/helloworldmobileapp/MainActivity.kt
@@ -0,0 +1,27 @@
+ package com.peacecodes.helloworldmobileapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+
+ class MainActivity : AppCompatActivity() {
+    private var count : Int = 0
+
+     override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+     }
+
+     fun onDecreaseClick(view: View) {
+         count++
+         val textView = findViewById<TextView>(R.id.textView)
+         textView.text = " $count"
+     }
+
+     fun onIncreaseClick(view: View) {
+         count--
+         val textView = findViewById<TextView>(R.id.textView)
+         textView.text = "  $count"
+     }
+ }
